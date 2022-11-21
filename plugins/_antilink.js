7a055e2702a82692415e1bb7bd6323295fae89b8
ibengar <56824876+ibengar@users.noreply.github.com>
2022-11-21 22:59:46 +0700

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,24 +1,19 @@
-let handler = m => m
+const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
+export async function before(m, { conn, isAdmin, isBotAdmin }) {
+	if (m.isBaileys && m.fromMe)
+		return !0
+	if (!m.isGroup) return !1
+	let chat = db.data.chats[m.chat]
+	let bot = db.data.settings[this.user.jid] || {}
+	const isGroupLink = linkRegex.exec(m.text)
 
-let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
-handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
-  if ((m.isBaileys && m.fromMe) || m.fromMe || !m.isGroup) return true
-  let chat = global.db.data.chats[m.chat]
-  let isGroupLink = linkRegex.exec(m.text)
-
-  if (chat.antiLink && isGroupLink) {
-    await m.reply(`*「 ANTI LINK 」*\n\nDetected *${await conn.getName(m.sender)}* you have sent the group link!\n\nSorry you will be kicked from this group byee!`)
-    if (isAdmin) return m.reply('*Eh sorry you admins, you will not be kicked. hehe..*')
-    if (!isBotAdmin) return m.reply('*Bots are not admins, how can they kick people _-*')
-    let linkGC = ('https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat))
-    let isLinkconnGc = new RegExp(linkGC, 'i')
-    let isgclink = isLinkconnGc.test(m.text)
-    if (isgclink) return m.reply('*「 ANTI LINK 」*\n\nOrder denied, bot will not kick you.\nBecause the group link itself')
-    await conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
-  }
-  return true
+	if (chat.antiLink && isGroupLink && !isAdmin) {
+		if (isBotAdmin) {
+			const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
+			if (m.text.includes(linkThisGroup)) return !0
+		}
+		if (!m.key.fromMe && isBotAdmin) await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: m.id, participant: m.sender } })
+		await conn.sendButton(m.chat, `*Group link detect!*${isBotAdmin ? '' : '\n\n_Bot not admin_  t_t'}`, packname + " - " + author, ['off antilink', '/disable antilink'], m)
+	}
+	return !0
 }
-
-module.exports = handler
-
-
